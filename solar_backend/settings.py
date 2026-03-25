28b0f40b923701d1ec0edeea23b6490ec60353ee
mayowa <joshuamayowa907@gmail.com>
2026-03-25 05:23:16 +0100

    fixed middlware sequence and syntax

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import EMAIL_HOST, STATIC_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-_y+hmwkh%q!u-z20m*7y=)h#rj!i@%#r5l&4lu2da92*m&@ew5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
