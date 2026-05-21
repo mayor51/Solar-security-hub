a8d44c80c1617f3fdd65e34115243872c3ccf407
mayowa <joshuamayowa907@gmail.com>
2026-05-21 14:08:14 +0100

    update secret key to use environment variable

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_y+hmwkh%q!u-z20m*7y=)h#rj!i@%#r5l&4lu2da92*m&@ew5'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-here')
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
