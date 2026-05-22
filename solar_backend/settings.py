b6868993e73da84d8422293687e2b1d64b463676
mayowa <joshuamayowa907@gmail.com>
2026-05-22 19:05:42 +0100

    modified settings.py

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -21,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-here')
+SECRET_KEY =  os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-here')
 
-DEBUG = True
-ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://solar-security-hub.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
