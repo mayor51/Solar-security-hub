bf00b2160e1fb4558f7bffed9e6785a8a05b7bdd
mayowa <joshuamayowa907@gmail.com>
2026-06-01 11:46:27 +0100

    update allowed host for production

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://solar-security-hub.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'solar-security-hub.onrender.com']
 
 
 
