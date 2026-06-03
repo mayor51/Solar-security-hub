e225dee3d34d02423d408e5686e833eccdeb4e2a
mayowa <joshuamayowa907@gmail.com>
2026-06-03 15:47:58 +0100

    switch to postgresql & update requirements

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'solar_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+    import dj_database_url
+    import os
+    #this locks for the DATABASE_URL environment variable from render
+    #if it cant find it (like when you are working locally),it falls back to SQLite
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        defualt=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
